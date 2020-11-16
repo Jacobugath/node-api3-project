814a3e10473b2375b11ffb9d30ee2c619cf163fa
B-Stock Admin <jacobthomasmorris.com>
2020-11-15 19:45:03 -0700

    done

--- server.js
+++ server.js
@@ -2,12 +2,27 @@ const express = require('express');
 
 const server = express();
 
+
+const userRouter = require('./users/userRouter');
+
+
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
+  
 });
+ 
+server.listen(5000);
 
-//custom middleware
+server.use(express.json());
+server.use(logger);
+server.use('/api/users', userRouter)
 
-function logger(req, res, next) {}
+
+function logger(req, res, next) {
+  console.log(req.method, req.url,`[${new Date().toISOString()}]`);
+  next();
+}
+
+
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -2,46 +2,63 @@ const express = require('express');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  // do your magic!
-});
 
-router.post('/:id/posts', (req, res) => {
-  // do your magic!
+const {
+  get,
+  getById,
+  insert,
+  update,
+  remove,
+} = require('./userDb');
+
+router.post('/', validateUser, (req, res) => {
+  insert(req.body).then(a => res.send(a));
+});
+  
+router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
+  insert(req.body).then(a => res.send(a));
 });
 
 router.get('/', (req, res) => {
-  // do your magic!
+  get().then(a => res.send(a));
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get('/:id', validateUserId, (req, res) => {
+  const {id} = req.params;
+  getById(id).then(a => res.send(a));
 });
 
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
+
+router.delete('/:id', validateUserId, (req, res) => {
+  const {id} = req.params;
+  remove(id).then(a => res.send('deleted'));
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put('/:id', validateUserId, (req, res) => {
+  const {id} = req.params;
+  update(id, req.body).then(a => res.send('updated'))
 });
 
 //custom middleware
 
-function validateUserId(req, res, next) {
-  // do your magic!
+function validateUserId(req, res, next) {  
+    getById(req.params.id).then(a => {
+      if(a === undefined) res.status(404).send('no such user')
+      req.user = a;
+    }).catch(a => console.log('no', a))
+  next();
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
+  if(!req.body) res.status(400).send({message: "missing user data"})
+  if(!req.body.name) res.status(400).send({ message: "missing required name field" })
+  next();
 }
 
 function validatePost(req, res, next) {
-  // do your magic!
+  if(!req.body) res.status(400).send({message: "missing post data"})
+  if(!req.body.text) res.status(400).send({ message: "missing required text field" })
+  next();
 }
 
 module.exports = router;
